2918031d10ee739c220fd298c5a6f08385f2335f
Juliana <mariajulianavianaliberato@gmail.com>
2026-09-21 15:15:45 -0300

    linkei com o css

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="calcu.css">
+    <link rel="stylesheet" href="index.css">
     <title>Calculadora</title>
 </head>
 <body>
